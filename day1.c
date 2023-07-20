e4da614c115f57b5cb6b35d5aa6b0188cf811482
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-07-20 10:50:04 +0530

    D1 P1,2

--- day1.c
+++ day1.c
@@ -1,32 +1,37 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-#define MAX 10
 
 int main() {
   FILE* inputFile = fopen("./input1.txt", "r");
 
+  int depths[2048]; // each elem is a number from the file
+  int lines = 0;    // will contain number of elems in depths
+
+  char tempLine[15];
+  while (fgets(tempLine, 15, inputFile) != NULL) {
+    depths[lines] = atoi(tempLine);
+    lines++;
+  }
+
+  // Part one
   int inc = 0;
-  char line1[MAX];
-  char line2[MAX] = "";
-
-  while (
-    fgets(line1, MAX, inputFile) != NULL
-  ) {
-
-    if (strcmp(line2, "") == 0) {
-      strcpy(line2, line1);
-      continue;
-    };
-
-    int depth1 = atoi(line1);
-    int depth2 = atoi(line2);
-
-    if (depth2 < depth1) inc++;
-    strcpy(line2, line1);
+  for (int i = 1; i < lines; i++) {
+    if (depths[i] > depths[i - 1]) { inc++; }
   }
 
   printf("%i\n", inc);
 
+  // Part two
+  int windowInc = 0;
+  for (int i = 1; i < lines - 2; i++) {
+    int window_1 = depths[i - 1] + depths[i] + depths[i + 1];
+    int window_2 = depths[i] + depths[i + 1] + depths[i + 2];
+
+    if (window_2 > window_1) { windowInc++; }
+  }
+  printf("%i\n", windowInc);
+
+  fclose(inputFile);
   return 0;
 }
