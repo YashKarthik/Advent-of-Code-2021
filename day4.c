47f2365ea3208f980599ac471bdfa7ea3eb3a86e
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-07-26 18:33:03 +0530

    stuff

--- day4.c
+++ day4.c
@@ -85,3 +85,37 @@ int main() {
   fclose(f);
   return 0;
 }
+
+int checkWin(BingoBoard *bingoBoards, int numOfBoards) {
+
+  for (int boardIdx = 0; boardIdx < numOfBoards; boardIdx++) {
+    for (int rowStart = 0; rowStart < (ROWS * COLS) - 5; rowStart+=5) {
+
+      int markedInRow = 0;
+      if (bingoBoards[boardIdx].numOfMarkedCells < 5) return -1;
+
+      if (
+          bingoBoards[boardIdx].cells[rowStart].marked == true &&
+          bingoBoards[boardIdx].cells[rowStart+1].marked == true &&
+          bingoBoards[boardIdx].cells[rowStart+2].marked == true &&
+          bingoBoards[boardIdx].cells[rowStart+3].marked == true &&
+          bingoBoards[boardIdx].cells[rowStart+4].marked == true
+         ) return boardIdx;
+    }
+
+    //for (int colIdx = 0; colIdx < (ROWS * COLS) - 5; colIdx+=5) {
+
+    //  int markedInRow = 0;
+    //  if (bingoBoards[boardIdx].numOfMarkedCells < 5) return -1;
+
+    //  if (
+    //      bingoBoards[boardIdx].cells[colIdx].marked == true &&
+    //      bingoBoards[boardIdx].cells[colIdx+1].marked == true &&
+    //      bingoBoards[boardIdx].cells[colIdx+2].marked == true &&
+    //      bingoBoards[boardIdx].cells[colIdx+3].marked == true &&
+    //      bingoBoards[boardIdx].cells[colIdx+4].marked == true
+    //     ) return boardIdx;
+    //}
+  }
+
+}
