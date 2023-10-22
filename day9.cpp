acd28e9b589d6824e459535610930c7897eb75ca
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-10-22 19:14:39 -0400

    minor change

--- day9.cpp
+++ day9.cpp
@@ -30,8 +30,8 @@ int main() {
     std::cout << "Couldn't open file.";
   }
 
-  for (int i{0}; i < 5; i++) {
-    for (int j{0}; j < 10; j++) {
+  for (int i{0}; i < numRows; i++) {
+    for (int j{0}; j < numCols; j++) {
       if (isLowPoint(heightMap, i, j, numCols, numRows)) {
         std::cout << "\e[1m\033[31m"; 
 
@@ -51,22 +51,22 @@ int isLowPoint(int (*heightMap)[N], int currRow, int currCol, int numCols, int n
 
   // test right
   if (currCol + 1 >= numCols) fails++;
-  else if (heightMap[currRow][currCol] > heightMap[currRow][currCol + 1]) return 0;
+  else if (heightMap[currRow][currCol] >= heightMap[currRow][currCol + 1]) return 0;
   else fails++;
 
   // test left
   if (currCol == 0) fails++;
-  else if (heightMap[currRow][currCol] > heightMap[currRow][currCol - 1]) return 0;
+  else if (heightMap[currRow][currCol] >= heightMap[currRow][currCol - 1]) return 0;
   else fails++;
 
   // test top
   if (currRow == 0) fails++;
-  else if (heightMap[currRow][currCol] > heightMap[currRow - 1][currCol]) return 0;
+  else if (heightMap[currRow][currCol] >= heightMap[currRow - 1][currCol]) return 0;
   else fails++;
 
   // test top
   if (currRow + 1 >= numRows) fails++;
-  else if (heightMap[currRow][currCol] > heightMap[currRow + 1][currCol]) return 0;
+  else if (heightMap[currRow][currCol] >= heightMap[currRow + 1][currCol]) return 0;
   else fails++;
 
   return fails == 4 ? 1 : 0;
